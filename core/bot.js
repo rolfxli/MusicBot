cb87871389aca9c4909b9e9dc17c0a6dcfcddcdb
Rolf Li <rolf.x.li@gmail.com>
2019-07-17 16:32:49 -0400

    non-working commit; structure complete

--- core/bot.js
+++ core/bot.js
@@ -1,1 +1,43 @@
-// main file for bot.js - the core of the discord.js library
+'use strict'
+
+const Discord = require('discord.js');
+const musicplayer = require('./musicplayer')
+const playlist = require('./playlist');
+const song = require('./song');
+
+class Bot {
+    constructor() {
+        this.client = new Discord.Client()
+    }
+
+    join(id) {
+
+    }
+
+    leave() {
+
+    }
+
+    play(song) {
+
+    }
+
+    pause() {
+
+    }
+
+    stop() {
+
+    }
+
+    skip() {
+
+    }
+
+    shuffle() {
+
+    }
+
+
+
+ }
