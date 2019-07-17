dcb39b6a2d3ef384788b883e8df8a146bb71740e
Rolf Li <rolf.x.li@gmail.com>
2019-07-17 14:48:56 -0400

    initial commit: moved token to .env

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+require('dotenv').config()
+
 const Discord = require('discord.js')
+const client = new Discord.Client()
+
+client.on('ready', () => {
+    console.log('Logged in')
+});
+
+client.on('message', msg => {
+    if (msg.content == 'ping') {
+        msg.reply('Pong')
+    }
+});
+
+client.login(process.env.BOT_TOKEN)
 
 // index to be developed
